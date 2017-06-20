d72ca8f0dc8336b32c36195f5c04d8c2e8c18429
System <none>
2017-06-20 12:42:02 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ var app = express();
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // init Spotify API wrapper
-
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var redirectUri = 'https://spotify-easyauth.glitch.me/callback';
-var tokenExpirationEpoch;
+var redirectUri = 'https://spotify-client-credentials-node.glitch.me/callback';
 
 // The API object we'll use to interact with the API
 var spotifyApi = new SpotifyWebApi({
@@ -22,7 +20,7 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri : redirectUri
 });
 
-// Using the Client Credentials Flow, authenticate our app
+// Using the Client Credentials auth flow, authenticate our app
 spotifyApi.clientCredentialsGrant()
   .then(function(data) {
     console.log('The access token expires in ' + data.body['expires_in']);
@@ -41,34 +39,6 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/authorize", function (request, response) {
-  var scopes = ['user-read-currently-playing'];
-  var authorizeURL = spotifyApi.createAuthorizeURL(scopesArray);
-  console.log(authorizeURL)
-  response.send(authorizeURL);
-});
-
-// Exchange Authorization Code for an Access Token
-app.get("/callback", function (request, response) {
-  var authorizationCode = request.query.code;
-  
-  response.sendFile(__dirname + '/views/callback.html');
-  
-  spotifyApi.authorizationCodeGrant(authorizationCode)
-  .then(function(data) {
-
-    // Set the access token and refresh token
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi.setRefreshToken(data.body['refresh_token']);
-
-    // Save the amount of seconds until the access token expired
-    tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
-    console.log('Retrieved token. It expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
-  }, function(err) {
-    console.log('Something went wrong when retrieving the access token!', err.message);
-  });
 });
 
 // listen for requests :)
