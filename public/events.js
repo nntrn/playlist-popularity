a5cd71448352c7fa5b58858bfdde03c67445fba1
Glitch (spotify-playground) <none>
2019-11-03 02:58:20 +0000

    🏰🚞 Checkpoint ./public/events.js:2682615/3282 ./views/index.html:2682615/9

--- public/events.js
+++ public/events.js
@@ -1,22 +1,30 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  
-  const theme={
-    primary: '#22dd64',
-    accent: '#1DB954'
-  }
-  
-  document.querySelectorAll('[data-bg]').forEach(e=>{
-    e.style.background = theme[e.dataset.bg]
-    e.style.border = `1px solid ${theme[e.dataset.bg]}`
-  })
-  
-  document.querySelectorAll('form').forEach(e=>{
-    e.addEventListener('submit', function(ev){
-      ev.preventDefault()
-      console.log(ev)
+window.addEventListener("DOMContentLoaded", event => {
+  const theme = {
+    primary: "#22dd64",
+    accent: "#1DB954"
+  };
+
+  document.querySelectorAll("[data-bg]").forEach(e => {
+    e.style.background = theme[e.dataset.bg];
+    e.style.border = `1px solid ${theme[e.dataset.bg]}`;
+  });
+
+  // document.querySelectorAll('form').forEach(e=>{
+  //   e.addEventListener('submit', function(ev){
+  //     ev.preventDefault()
+  //     console.log(ev)
+  //   });
+  // })
+
+  document.querySelectorAll("[data-api]").forEach(e => {
+    e.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      console.log(ev.target.dataset.api);
+
+      $.get("/user-playlist", function(data) {
+        e.nextSibling.innerHTML =
+          "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+      });
     });
-  })
-  
-  
-  
+  });
 });
