91fbd7476fb2f7b7c1aa65137a3ecdc910ad320b
Glitch (spotify-playground) <none>
2019-11-03 07:32:16 +0000

    👶👬 Checkpoint ./views/index.html:2682615/107 ./package.json:2682615/102 ./public/events.js:2682615/1583 ./.prettierc:2682615/87

--- public/events.js
+++ public/events.js
@@ -1,23 +1,21 @@
-window.addEventListener("DOMContentLoaded", event => {
-  document.querySelectorAll("[data-api]").forEach(e => {
-    e.addEventListener("submit", function(ev) {
-      ev.preventDefault();
+document.querySelectorAll("[data-api]").forEach(e => {
+  e.addEventListener("submit", function(ev) {
+    ev.preventDefault();
 
-      const query = Array.from(ev.target.querySelectorAll("[name]"))
-        .map(e => `${e.name}=${e.value}`)
-        .join("&");
-      const apiPath = `${ev.target.dataset.api}?${query}`;
+    const query = Array.from(ev.target.querySelectorAll("[name]"))
+      .map(e => `${e.name}=${e.value}`)
+      .join("&");
+    const apiPath = `${ev.target.dataset.api}?${query}`;
 
-      $.get(apiPath, function(data) {
-        let pre = document.createElement("pre");
-        pre.textContent = JSON.stringify(data, null, 2);
-        Object.assign(pre, {
-          textContent: JSON.stringify(data, null, 2),
-          style:
-            "max-height:300px;overflow-y:scroll;background:rgba(255,255,255,.6);padding:.25rem"
-        });
-        ev.target.parentElement.querySelector(".output").appendChild(pre);
+    $.get(apiPath, function(data) {
+      let pre = document.createElement("pre");
+      pre.textContent = JSON.stringify(data, null, 2);
+      Object.assign(pre, {
+        textContent: JSON.stringify(data, null, 2),
+        style:
+          "max-height:300px;overflow-y:scroll;background:rgba(255,255,255,.6);padding:.25rem"
       });
+      ev.target.parentElement.querySelector(".output").appendChild(pre);
     });
   });
 });
