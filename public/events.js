aa6140b36419282aff0b412922e5b1d2ce8f2bf2
Glitch (spotify-api-viewer) <none>
2019-11-03 09:32:15 +0000

    👓🍧 Checkpoint ./public/events.js:2682615/1843

--- public/events.js
+++ public/events.js
@@ -6,22 +6,22 @@ document.querySelectorAll("[data-api]").forEach(e => {
       .map(e => `${e.name}=${e.value}`)
       .join("&");
     const apiPath = `${ev.target.dataset.api}?${query}`;
-    
-    console.log(apiPath)
-    
+
+    console.log(apiPath);
+
     $.get(apiPath, function(data) {
       let pre = document.createElement("pre");
-      let link = document.createElement("a")
+      let link = document.createElement("a");
 
       Object.assign(pre, {
         textContent: JSON.stringify(data, null, 2),
         style:
           "max-height:300px;overflow-y:scroll;background:rgba(255,255,255,.6);padding:.25rem"
       });
-      Object.assign(link,{
+      Object.assign(link, {
         href: apiPath,
-        textContent:apiPath
-      })
+        textContent: apiPath
+      });
       ev.target.parentElement.querySelector(".output").appendChild(link);
       ev.target.parentElement.querySelector(".output").appendChild(pre);
     });
