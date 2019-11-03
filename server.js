8b5d8697aa92f19f3b58448e2f43582ac54a0941
Glitch (spotify-playground) <none>
2019-11-03 07:56:17 +0000

    🐓🌘 Checkpoint ./package.json:2682615/32 ./server.js:2682615/64

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get("/search-playlist", function(req, res) {
 
   spotifyApi.searchPlaylists(keyword, { limit: limit, offset: offset }).then(
     function(data) {
-      // Object.assign(data.body, { api: req.url });
+      Object.assign(data.body, { api: req.url });
       res.send(data.body);
     },
     function(err) {
@@ -62,6 +62,6 @@ app.get("/search-playlist", function(req, res) {
 
 /* WEB SERVER */
 const listener = app.listen(process.env.PORT, function() {
-  console.log(process.env.ENVIRONMENT)
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log(JSON.stringify(listener,null,2))
+  // console.log("Your app is listening on port " + listener.address().port);
 });
