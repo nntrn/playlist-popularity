ce866b799d42cb901a81e2a8421f608c90967691
Glitch (spotify-playground) <none>
2019-11-03 07:08:18 +0000

    💁👹 Checkpoint ./package.json:2682615/31 ./public/style.css:2682615/171 ./views/index.html:2682615/135 ./public/events.js:2682615/2327

--- public/events.js
+++ public/events.js
@@ -1,32 +1,22 @@
 window.addEventListener("DOMContentLoaded", event => {
-  const theme = {
-    primary: "#22dd64",
-    accent: "#1DB954"
-  };
-
-  document.querySelectorAll("[data-bg]").forEach(e => {
-    e.style.background = theme[e.dataset.bg];
-    e.style.border = `1px solid ${theme[e.dataset.bg]}`;
-  });
-
-
   document.querySelectorAll("[data-api]").forEach(e => {
     e.addEventListener("submit", function(ev) {
       ev.preventDefault();
-      
-      const query = Array.from(ev.target.querySelectorAll('[name]')).map(e=>`${e.name}=${e.value}`).join('&')
-      const apiPath = `${ev.target.dataset.api}?${query}`
-      
+
+      const query = Array.from(ev.target.querySelectorAll("[name]"))
+        .map(e => `${e.name}=${e.value}`)
+        .join("&");
+      const apiPath = `${ev.target.dataset.api}?${query}`;
+
       $.get(apiPath, function(data) {
-        let pre = document.createElement('pre')
-        pre.textContent = JSON.stringify(data, null, 2)
+        let pre = document.createElement("pre");
+        pre.textContent = JSON.stringify(data, null, 2);
         Object.assign(pre, {
-          textContent:JSON.stringify(data, null, 2),
-          style:'max-height:300px;overflow-y:scroll;background:rgba(255,255,255,.6);padding:.25rem'
-          
-        })
-        ev.target.parentElement.querySelector('.output').appendChild(pre)
-  
+          textContent: JSON.stringify(data, null, 2),
+          style:
+            "max-height:300px;overflow-y:scroll;background:rgba(255,255,255,.6);padding:.25rem"
+        });
+        ev.target.parentElement.querySelector(".output").appendChild(pre);
       });
     });
   });
