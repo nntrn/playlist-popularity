c0eca27f4fbf4bb190bc0ad3c238d9505f6c2027
Glitch (spotify-user-playlist) <none>
2020-02-14 00:02:19 +0000

    📰🏔 Checkpoint ./views/user.hbs:2682615/9267 ./public/style.css:2682615/381 ./server.js:2682615/1

--- server.js
+++ server.js
@@ -83,7 +83,6 @@ app.get("/api/user/:user", function(req, res) {
     });
 });
 
-
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
